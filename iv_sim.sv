1c50860b190c1edc360de5a54c8a929b974631d2
primenumber <prime@kmc.gr.jp>
2021-04-30 12:10:47 +0900

    Update sim

--- iv_sim.sv
+++ iv_sim.sv
@@ -7,7 +7,12 @@ reg iCLOCK;
 reg enable;
 reg [63:0] iPlayer;
 reg [63:0] iOpponent;
-logic signed [7:0] expected;
+reg [63:0] iPlayer0;
+reg [63:0] iOpponent0;
+logic signed [7:0] expected0;
+reg [63:0] iPlayer1;
+reg [63:0] iOpponent1;
+logic signed [7:0] expected1;
 logic solved;
 logic [63:0] oPlayer;
 logic [63:0] oOpponent;
@@ -25,19 +30,27 @@ pipeline pipeline(
   .res(res),
   .o(o));
 
+reg parity;
+
 integer i, j;
 time stones;
 task tsk_check;
   begin
-    //iPlayer <= 64'h10B8DDE3B1B98284;
-    //iOpponent <= 64'h8E45221C4E467C78;
-    //expected <= 16;
-    iPlayer <= 64'h001F03070B15FF01;
-    iOpponent <= 64'h7F207CF8F4EA00FE;
-    expected <= 14;
-    //iPlayer <= 64'hBF8387EBB3F8C002;
-    //iOpponent <= 64'h407C78144C073F3D;
-    //expected <= 2;
+    //iPlayer0 <= 64'h10B8DDE3B1B98284;
+    //iOpponent0 <= 64'h8E45221C4E467C78;
+    //expected0 <= 16;
+    iPlayer1 <= 64'h10B8DDE3B1B98284;
+    iOpponent1 <= 64'h8E45221C4E467C78;
+    expected1 <= 16;
+    iPlayer0 <= 64'h001F03070B15FF01;
+    iOpponent0 <= 64'h7F207CF8F4EA00FE;
+    expected0 <= 14;
+    //iPlayer1 <= 64'h001F03070B15FF01;
+    //iOpponent1 <= 64'h7F207CF8F4EA00FE;
+    //expected1 <= 14;
+    //iPlayer1 <= 64'hBF8387EBB3F8C002;
+    //iOpponent1 <= 64'h407C78144C073F3D;
+    //expected1 <= 2;
 
     for (i = 0; i < 8; i = i+1) begin
       #PL_CYCLE;
@@ -45,10 +58,13 @@ task tsk_check;
     end
     
     $display("!!!!!start!!!!!");
-    stones = iPlayer | iOpponent;
-    $display("%h", stones);
+    //stones = iPlayer | iOpponent;
+    $display("B0: %h %h", iPlayer0, iOpponent0);
+    $display("B1: %h %h", iPlayer1, iOpponent1);
     
     enable <= 1'b1;
+    #PL_CYCLE;
+    $display("B: %h %h", iPlayer, iOpponent);
     
     for (i = 0; i < 300000; i = i+1) begin
       #PL_CYCLE;
@@ -56,7 +72,7 @@ task tsk_check;
       if (solved == 1'b1) begin
         $display("solved!");
         $display("%h %h %d %d", oPlayer, oOpponent, res, o);
-        if (res != expected) begin
+        if (res != expected0 && res != expected1) begin
           $display("wrong answer");
         end else begin
           $display("collect answer");
@@ -68,14 +84,28 @@ task tsk_check;
 endtask
 
 initial begin
+  forever begin
+    #PL_CYCLE;
+    if (parity) begin
+      iPlayer <= iPlayer1;
+      iOpponent <= iOpponent1;
+    end else begin
+      iPlayer <= iPlayer0;
+      iOpponent <= iOpponent0;
+    end
+    parity <= ~parity;
+  end
+end
+
+initial begin
   iCLOCK = 1'b0;
+  parity = 1'b1;
   forever begin
     #(PL_CYCLE / 2) iCLOCK = ~iCLOCK;
   end
 end
 
 initial begin
-  iCLOCK = 1'b0;
   enable = 1'b0;
 
   #(PL_CYCLE * 5);
